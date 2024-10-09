bbf5ee908d81df5748917e8a82869c2cb0dfd3e3
kantsyr <katia.kantsyr@gmail.com>
2024-10-10 00:08:50 +0300

    Taking into account new conditions for hp

--- src/main/java/lotr/Character.java
+++ src/main/java/lotr/Character.java
@@ -3,13 +3,16 @@ package lotr;
 public abstract class Character {
     protected int power;
     protected int hp;
-
+ 
     public abstract void kick(Character c);
 
     public boolean isAlive() {
         return hp > 0;
     }
 
+    public void setHp(int hp) {
+        this.hp = Math.max(0, hp);
+    }
     public int getHp() {
         return hp;
     }
